5936b18684e46479486633475e0e3ef6a5c4a998
@kanhapro <kanhakumarkhatua2001@gmail.com>
2025-07-18 15:48:36 +0530

    fix: adjust container height on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full ">
+    <div className="flex items-center justify-center h-[75vh] w-full ">
       <LoginForm />
     </div>
   );
