b4831149ac6537f7d4084a2bff5fd2ba8604b5fe
@kanhapro <kanhakumarkhatua2001@gmail.com>
2025-07-26 10:11:54 +0530

    fix: changes

--- src/services/doctor.api.ts
+++ src/services/doctor.api.ts
@@ -17,3 +17,19 @@ export const getAssignedAppointments = async (
     throw new Error("Unexpected error occurred.");
   }
 };
+
+export const getAssignedAppointmentDtlsById = async (
+  appointment_id: string | string[]
+) => {
+  try {
+    const response = await api.get(`appointment/single/${appointment_id}`);
+
+    return response.data;
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      const message = error?.message || "Token generation failed";
+      throw new Error(message);
+    }
+    throw new Error("Unexpected error occurred.");
+  }
+};
--- src/types/doctor.types.ts
+++ src/types/doctor.types.ts
@@ -1,3 +1,24 @@
 export interface doctorSliceInitialStates {
   confirmConsultationModalVisible: boolean;
 }
+
+interface VitalObservation {
+  vital_definition: {
+    name: string;
+  };
+  value: {
+    value: string;
+  };
+}
+
+interface PreQa {
+  questionary: {
+    question: string;
+  };
+  answer: string | null;
+}
+
+export interface AppointmentDetails {
+  vital_observations: VitalObservation[];
+  pre_qa: PreQa[];
+}
